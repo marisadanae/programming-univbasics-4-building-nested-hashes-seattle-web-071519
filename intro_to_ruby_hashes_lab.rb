b202068e17637705dff17acd9c4154231fccf8ad
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-06 22:59:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,18 +1,18 @@
 def monopoly
 	monopoly = {
-		:railroads => 
+		:railroads =>
 		{
 			:pieces =>
 			:rent_in_dollars =>
 		{
-		
+
 	}}
 end
 
 def monopoly_with_second_tier
 	monopoly = {
 		:railroads=>
-		
+
 	}
 
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
